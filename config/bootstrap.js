a01edc43ee326a092fa39f28cb93fe778a4b77cf
Renan Martins <martins@fujisan.co.jp>
2017-11-17 12:37:00 -0800

    Create fixture records in bootstrap

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,7 +11,42 @@
 
 module.exports.bootstrap = function(cb) {
 
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  Model.destroy({})
+    .then(() => {
+      return Product.destroy({});
+    })
+    .then(() => {
+      return Maker.destroy({});
+    })
+    .then(() => {
+      return Promise.all([
+        Maker.create({ name: 'Maker ' + Math.random().toFixed(2) }),
+        Maker.create({ name: 'Maker ' + Math.random().toFixed(2) }),
+      ]);
+    })
+    .then(makers => {
+      return Promise.all([
+        Product.create({ name: 'Product ' + Math.random().toFixed(4), maker: makers[0].id }),
+        Product.create({ name: 'Product ' + Math.random().toFixed(4), maker: makers[1].id }),
+        Product.create({ name: 'Product ' + Math.random().toFixed(4), maker: makers[0].id }),
+        Product.create({ name: 'Product ' + Math.random().toFixed(4), maker: makers[1].id }),
+      ]);
+    })
+    .then(products => {
+      return Promise.all([
+        Model.create({ name: 'Model ' + Math.random().toFixed(3), price: (Math.random() * 100).toFixed(2), releaseDate: '2014-03-17', product: products[0].id }),
+        Model.create({ name: 'Model ' + Math.random().toFixed(3), price: (Math.random() * 100).toFixed(2), releaseDate: '2017-11-17', product: products[0].id }),
+        Model.create({ name: 'Model ' + Math.random().toFixed(3), price: (Math.random() * 100).toFixed(2), releaseDate: '2016-04-17', product: products[1].id }),
+        Model.create({ name: 'Model ' + Math.random().toFixed(3), price: (Math.random() * 100).toFixed(2), releaseDate: '2017-11-17', product: products[1].id }),
+        Model.create({ name: 'Model ' + Math.random().toFixed(3), price: (Math.random() * 100).toFixed(2), releaseDate: '2017-11-17', product: products[2].id }),
+        Model.create({ name: 'Model ' + Math.random().toFixed(3), price: (Math.random() * 100).toFixed(2), releaseDate: '2014-10-17', product: products[3].id }),
+        Model.create({ name: 'Model ' + Math.random().toFixed(3), price: (Math.random() * 100).toFixed(2), releaseDate: '2016-05-17', product: products[3].id }),
+      ]);
+    })
+    .then(() => {
+      // It's very important to trigger this callback method when you are finished
+      // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+      cb();
+    });
+
 };
