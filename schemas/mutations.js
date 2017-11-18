acb023c3a262f4fe81807cfcfc5ecd3eb5808231
Renan Martins <martins@fujisan.co.jp>
2017-11-17 16:01:30 -0800

    Add updateMaker mutation

--- schemas/mutations.js
+++ schemas/mutations.js
@@ -1,6 +1,8 @@
 const {
   GraphQLObjectType,
   GraphQLID,
+  GraphQLNonNull,
+  GraphQLString,
 } = require('graphql');
 
 module.exports = new GraphQLObjectType({
@@ -10,11 +12,28 @@ module.exports = new GraphQLObjectType({
       type: require('./MakerType'),
       args: {
         id: {
-          type: GraphQLID,
+          type: new GraphQLNonNull(GraphQLID),
         },
       },
       resolve(parentValue, args) {
         return Maker.destroy({ id: args.id });
+      },
+    },
+    updateMaker: {
+      type: require('./MakerType'),
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+        },
+        name: {
+          type: GraphQLString,
+        },
+      },
+      resolve(parentValue, { id, name }) {
+        console.log('Update Maker!!!');
+        return Maker.update({ id }, { name }).then(updatedRecords => {
+          return updatedRecords[0];
+        });
       },
     }
   },
